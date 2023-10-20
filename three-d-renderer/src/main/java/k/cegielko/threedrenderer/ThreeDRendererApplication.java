f321b42e7feb1ad83e0d46521609f21668758213
Karol Cegiełko <kcegielko31@gmail.com>
2023-10-20 12:03:07 +0200

    Initial commit

---
+++ three-d-renderer/src/main/java/k/cegielko/threedrenderer/ThreeDRendererApplication.java
@@ -0,0 +1,13 @@
+package k.cegielko.threedrenderer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeDRendererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreeDRendererApplication.class, args);
+	}
+
+}
---
+++ three-d-renderer/src/test/java/k/cegielko/threedrenderer/ThreeDRendererApplicationTests.java
@@ -0,0 +1,13 @@
+package k.cegielko.threedrenderer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeDRendererApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
